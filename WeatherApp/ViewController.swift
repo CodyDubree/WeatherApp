304c6e9cb2632da45276b0aefd4279816ef21d45
Cody Dubree <codydubree@MacBook-Pro-24.local>
2018-11-14 12:36:53 -0600

    pods installed

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Cody Dubree on 11/14/18.
+//  Copyright © 2018 Cody Dubree. All rights reserved.
+//
+
+import UIKit
+import SwiftyJSON
+import Alamofire
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
